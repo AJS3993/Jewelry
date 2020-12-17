c4d4037dd9058e452dadf97f32fe75f03a0133b9
Andrew Smith <ajs3993@gmail.com>
2020-12-17 07:10:36 -0600

    Updates

---
+++ precache-manifest.0b20290831ebdcc44e1edec68986a398.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee67a37c5676c505b102fd0bae05aeee",
+    "url": "/Jewelry/index.html"
+  },
+  {
+    "revision": "f374409eedd8ee0d414c",
+    "url": "/Jewelry/static/css/2.7f5e552d.chunk.css"
+  },
+  {
+    "revision": "83a41bc76dfc41c8829d",
+    "url": "/Jewelry/static/css/main.0be3a4c2.chunk.css"
+  },
+  {
+    "revision": "f374409eedd8ee0d414c",
+    "url": "/Jewelry/static/js/2.2a9e1adc.chunk.js"
+  },
+  {
+    "revision": "b46eae19120e0343c25f399c83a404d7",
+    "url": "/Jewelry/static/js/2.2a9e1adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a41bc76dfc41c8829d",
+    "url": "/Jewelry/static/js/main.5d7edd4d.chunk.js"
+  },
+  {
+    "revision": "fe78af56c555080450d5",
+    "url": "/Jewelry/static/js/runtime-main.e3672ae8.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Jewelry/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Jewelry/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/Jewelry/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Jewelry/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Jewelry/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Jewelry/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Jewelry/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Jewelry/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Jewelry/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Jewelry/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Jewelry/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Jewelry/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Jewelry/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Jewelry/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Jewelry/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Jewelry/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Jewelry/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Jewelry/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
